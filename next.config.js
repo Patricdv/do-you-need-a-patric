097477e7763d210dc39da5d74c1533a16160a2eb
Patricdv <patric.venturini@beatscode.com>
2020-11-05 08:22:14 -0300

    changed next config

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 // So, we write it in vanilla JS
 // (But you could use ES2015 features supported by your Node.js version)
 
-const debug = process.env.NODE_ENV !== 'production';
-
 module.exports = {
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-    };
-  },
-  assetPrefix: !debug ? 'https://patricdv.github.io/do-you-need-a-patric/' : '',
+  assetPrefix: '/do-you-need-a-patric/',
 };
