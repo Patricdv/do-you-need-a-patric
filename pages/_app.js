0a57013572e43a1b12d0a9afe5edbcb65c64741f
Patricdv <patric.venturini@beatscode.com>
2020-11-08 00:51:09 -0300

    Fixed favicon location

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/global.css';
 import '../styles/toasty.css';
@@ -5,8 +7,12 @@ import '../styles/depoiments.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <div>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,6 @@ export default function Home() {
     <>
       <Head>
         <title>Do you need a Patric?</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
