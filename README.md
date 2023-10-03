bdbf39d5e14425e12a30d7dff511fcf2f91a725c
Patric Venturini <patric.venturini@sky.uk>
2023-10-03 22:20:25 +0100

    (feat): adding oracle feature

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### OMG!!!!! DO YOU NEED A PATRIC?
 
-It's really my website? My very own personal website? I think it is! Such technology. Much WOOOW
+It's really my website? My very own personal website? I think it is! Such technology. Much WOOOW.
 
-Check it out.
+You can check it out on this  [:crystal_ball: :sparkles:Amazing Link:sparkles: :crystal_ball:](https://patricdv.github.io/do-you-need-a-patric/)
--- pages/dark-side/index.tsx
+++ pages/dark-side/index.tsx
@@ -1,14 +1,54 @@
 import Head from 'next/head';
+import { useState } from 'react';
+
+async function* generateSecret(): AsyncGenerator<string, undefined, string> {
+  try {
+    const res = await fetch('https://api.chucknorris.io/jokes/random');
+    const body = await res.json();
+    yield body.value.replaceAll('Chuck Norris', 'Patric');
+  } catch (e) {
+    yield `The Oracle didn't find any secret in his search. Maybe try again later.`
+  }
+}
+
+enum SECRET_STATUS {
+  LOADING,
+  DONE
+}
+
+interface Secret {
+  status: SECRET_STATUS;
+  value?: string
+}
 
 export default function DarkSide() {
+  const [secret, setSecret] = useState<Secret>({
+    status: SECRET_STATUS.DONE
+  });
+
+  const secretGenerator = generateSecret();
+
+  async function getSecret() {
+    setSecret({
+      status: SECRET_STATUS.LOADING
+    })
+    const nextSecret = (await secretGenerator.next());
+    if (nextSecret) {
+      setSecret({
+        status: SECRET_STATUS.DONE,
+        value: nextSecret.value
+      });
+    }
+  }
+
   return (
     <>
       <Head>
         <title>Do you need a Dark Patric?</title>
       </Head>
 
-      <main>
-        <section className="dark-side">
+      <main className="dark-side">
+        <section>
           <h2 data-text="Dark Side... of the Patric?">
             Dark Side... of the Patric?
           </h2>
@@ -19,7 +59,11 @@ export default function DarkSide() {
           </p>
         </section>
 
-        <section className="">Segunda seção</section>
+        <section className="oracle-section">
+          {secret.value ? <div className="patric-secrets">{secret.value}</div> : ''} 
+          
+          <button className="ask-oracle-button" onClick={getSecret} disabled={secret.status === SECRET_STATUS.LOADING}>Click here to find a new secret</button>
+        </section>
       </main>
     </>
   );
